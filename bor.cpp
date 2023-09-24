4dd59cf39cf79b34b4541362dfb241c40ca05664
Mathis Ribémont <mathisribemont@gmail.com>
2023-09-24 21:34:49 +0200

    first commit

---
+++ bor.cpp
@@ -0,0 +1,12 @@
+﻿// Bor Interpreter.cpp : définit le point d'entrée de l'application.
+//
+
+#include "bor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
