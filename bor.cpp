6f4e7cce9c386c48b560c1a95cacf2a16d4a06df
Mathis Ribémont <mathisribemont@gmail.com>
2023-09-25 11:43:15 +0200

    attempt to compile tree-sitter

--- bor.cpp
+++ bor.cpp
@@ -2,11 +2,37 @@
 //
 
 #include "bor.h"
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include "tree_sitter/api.h"
+#include "tree_sitter/parser.h"
 
 using namespace std;
 
+TSLanguage * tree_sitter_bor();
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	TSParser* parser = ts_parser_new();
+	
+	ts_parser_set_language(parser, tree_sitter_bor());
+
+	// Build a syntax tree based on source code stored in a string.
+	const char* source_code = "a + 2";
+	TSTree* tree = ts_parser_parse_string(
+		parser,
+		NULL,
+		source_code,
+		strlen(source_code)
+	);
+
+	// Get the root node of the syntax tree.
+	TSNode root_node = ts_tree_root_node(tree);
+
+	// Get some child nodes.
+	TSNode array_node = ts_node_named_child(root_node, 0);
+	TSNode number_node = ts_node_named_child(array_node, 0);
+
 	return 0;
 }
