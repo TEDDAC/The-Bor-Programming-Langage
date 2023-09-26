8cec429805a6ffa5fffd6dd7c040300257f9a0a9
TEDDAC <38114965+TEDDAC@users.noreply.github.com>
2023-09-26 13:47:42 +0200

    Dev/compilation chain (#1)
    * Compilation tool chain
    * CMake project
    * Import and compile tree-sitter lib
    * Working parsing example

--- bor.cpp
+++ bor.cpp
@@ -2,11 +2,46 @@
 //
 
 #include "bor.h"
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include "tree_sitter/api.h"
+#include "tree_sitter/parser.h"
 
 using namespace std;
 
+extern "C" TSLanguage * tree_sitter_bor(void);
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	TSParser* parser = ts_parser_new();
+	
+	ts_parser_set_language(parser, tree_sitter_bor());
+
+	// Build a syntax tree based on source code stored in a string.
+	const char* source_code = "a + 2";
+	TSTree* tree = ts_parser_parse_string(
+		parser,
+		NULL,
+		source_code,
+		strlen(source_code)
+	);
+
+	// Get the root node of the syntax tree.
+	TSNode root_node = ts_tree_root_node(tree);
+
+	// Get some child nodes.
+	TSNode array_node = ts_node_named_child(root_node, 0);
+	TSNode number_node = ts_node_named_child(array_node, 0);
+
+	// Print the syntax tree as an S-expression.
+	char* string = ts_node_string(root_node);
+	printf("Syntax tree: %s\n", string);
+
+	// Free all of the heap-allocated memory.
+	free(string);
+	ts_tree_delete(tree);
+	ts_parser_delete(parser);
+
 	return 0;
 }
